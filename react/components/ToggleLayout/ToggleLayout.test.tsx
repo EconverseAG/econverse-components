59e76b6c47167cda6f1cd2e374bedf82e083839b
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-18 17:40:06 -0300

    test(ToggleLayout): wrap testing cases around describe

--- react/components/ToggleLayout/ToggleLayout.test.tsx
+++ react/components/ToggleLayout/ToggleLayout.test.tsx
@@ -3,16 +3,18 @@ import { render } from '@vtex/test-tools/react';
 
 import ToggleLayout from './ToggleLayout';
 
-test('should render children if enabled', () => {
-  const { container } = render(<ToggleLayout enabled>Hello</ToggleLayout>);
+describe('ToggleLayout', () => {
+  it('should render children if enabled', () => {
+    const { container } = render(<ToggleLayout enabled>Hello</ToggleLayout>);
 
-  expect(container.innerHTML).toBe('Hello');
-});
+    expect(container.innerHTML).toBe('Hello');
+  });
 
-test('should not render children if disabled', () => {
-  const { container } = render(
-    <ToggleLayout enabled={false}>Hello</ToggleLayout>,
-  );
+  it('should not render children if disabled', () => {
+    const { container } = render(
+      <ToggleLayout enabled={false}>Hello</ToggleLayout>,
+    );
 
-  expect(container.innerHTML).toBe('');
+    expect(container.innerHTML).toBe('');
+  });
 });
