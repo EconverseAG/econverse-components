511e3478635160ad7f665ad57d51638010a57361
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-18 17:25:03 -0300

    refactor(ToggleLayout): remove testing console log

--- react/components/ToggleLayout/ToggleLayout.tsx
+++ react/components/ToggleLayout/ToggleLayout.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import ToggleLayoutSchema from './ToggleLayout.schema';
 import type ToggleLayoutProps from './ToggleLayout.types';
 
 function ToggleLayout({ enabled, children }: ToggleLayoutProps) {
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('>>> TESTEEE');
-  }, []);
-
   return <>{enabled && children}</>;
 }
 
