9b56c79b75797d56e3aaf76ce665e0904e2ac794
Eduardo Rodrigues <eduardo.rodrigues@econverse.com.br>
2022-05-29 12:26:32 -0300

    feat(ShippingSimulatorInput): add title prop

--- react/components/ShippingSimulator/ShippingSimulatorInput/ShippingSimulatorInput.types.ts
+++ react/components/ShippingSimulator/ShippingSimulatorInput/ShippingSimulatorInput.types.ts
@@ -3,6 +3,7 @@ import type { CssHandlesTypes } from 'vtex.css-handles';
 import type ShippingSimulatorInputHandles from './ShippingSimulatorInput.handles';
 
 interface ShippingSimulatorInputProps {
+  title?: string;
   placeholder?: string;
   cleanErrorsOnChange?: boolean;
   classes?: CssHandlesTypes.CustomClasses<typeof ShippingSimulatorInputHandles>;
